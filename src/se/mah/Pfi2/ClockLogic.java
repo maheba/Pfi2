ea5f91317f85432696153939e043e571c1322492
maheba <marianne.bansell@live.se>
2016-02-16 19:59:25 +0100

    Satt upp strukturen

---
+++ src/se/mah/Pfi2/ClockLogic.java
@@ -0,0 +1,9 @@
+package se.mah.Pfi2;
+
+public class ClockLogic {
+
+	public ClockLogic() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/se/mah/Pfi2/DigitalClockGUI.java
@@ -0,0 +1,9 @@
+package se.mah.Pfi2;
+
+public class DigitalClockGUI {
+
+	public DigitalClockGUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
